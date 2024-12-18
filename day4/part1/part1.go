8fab32704ae5c53707fafe2a9ac2f772b293a453
basilbarge <zachb.barge@gmail.com>
2024-12-18 12:50:20 -0500

    Started developing algorithm for getting forward diagonal values and replaced sample text with something easier to test

--- day4/part1/part1.go
+++ day4/part1/part1.go
@@ -9,8 +9,13 @@ import (
 
 func main() {
 	horizontalLines := util.ReadLines("../sample.txt")
-
 	verticalLines := make([]string, len(horizontalLines[0]))
+
+	numDiagonals := (len(horizontalLines) + len(horizontalLines[0]) - 1)
+
+	forwardDiagonal := make([]string, numDiagonals)
+	//backwardDiagonal := make([]string, numDiagonals)
+
 
 	regexp.MustCompile("XMAS|SAMX")
 
@@ -26,5 +31,24 @@ func main() {
 	fmt.Println("Vertical")
 	for _, vertLine := range verticalLines {
 		fmt.Println(vertLine)
+	}
+
+	for i := 0; i < numDiagonals; i++ {
+		count := i
+		forwardDiagonal[i] = string(horizontalLines[min(i, len(horizontalLines) - 1)][max(0, i - len(verticalLines))])
+
+		fmt.Printf("Iteration: %d\n", i)
+		for count != 0 {
+			y := min(count, len(horizontalLines) - 1) - 1
+			x := min(max(0, count - len(verticalLines)) + 1 + count, len(verticalLines) - 1)
+			fmt.Printf("Forward diag y: %d, Forward diag x: %d\n", y, x)
+			forwardDiagonal[i] += string(horizontalLines[x][y])
+			count--
+		}
+	}
+
+	fmt.Println("Forward Diagonal")
+	for _, forDiag := range forwardDiagonal {
+		fmt.Println(forDiag)
 	}
 }

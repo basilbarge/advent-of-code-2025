f20c83a1086add5b46fc2c2c0bb1eee78e5ab02e
basilbarge <zachb.barge@gmail.com>
2024-12-22 16:15:34 -0500

    Fixed algorithm and solved part 1 correctly

--- day4/part1/part1.go
+++ day4/part1/part1.go
@@ -1,95 +1,177 @@
+// Answer should be 2591
 package main
 
 import (
 	"fmt"
-	"regexp"
 
 	util "github.com/aoc2024/utils"
 )
 
 func main() {
-	horizontalLines := util.ReadLines("../input.txt")
-	verticalLines := make([]string, len(horizontalLines[0]))
+	lines := util.ReadLines("../input.txt")
 
-	numDiagonals := (len(horizontalLines) + len(horizontalLines[0]) - 1)
+	sum := 0
 
-	forwardDiagonal := make([]string, numDiagonals)
-	backwardDiagonal := make([]string, numDiagonals)
-
-
-	for _, line := range horizontalLines {
-		for idx, char := range line {
-			verticalLines[idx] += string(char)
+	for linesIdx, line := range lines {
+		for lineIdx, char := range line {
+			if char == 'X' {
+				sum += checkHorizontal(lines, linesIdx, lineIdx)
+				sum += checkHorizontalBackwards(lines, linesIdx, lineIdx)
+				sum += checkVertical(lines, linesIdx, lineIdx)
+				sum += checkVerticalBackwards(lines, linesIdx, lineIdx)
+				sum += checkForwardDiagonal(lines, linesIdx, lineIdx)
+				sum += checkForwardDiagonalBackwards(lines, linesIdx, lineIdx)
+				sum += checkBackDiagonal(lines, linesIdx, lineIdx)
+				sum += checkBackDiagonalBackwards(lines, linesIdx, lineIdx)
+			}
 		}
 	}
 
-	for idx := 0; idx < len(horizontalLines)+len(verticalLines)-1; idx++ {
-		xcount := 0
-		if idx > len(verticalLines) {
-			xcount = idx - len(verticalLines)
-		}
-		ycount := min(idx, len(horizontalLines)-1)
+	fmt.Println(sum)
 
-		forwardDiagonal[idx] = string(horizontalLines[ycount][xcount])
-
-		xcount += 1
-		ycount -= 1
-
-		for (xcount < len(verticalLines)) && (ycount >= 0) {
-			forwardDiagonal[idx] += string(horizontalLines[ycount][xcount])
-			xcount++
-			ycount--
-		}
-	}
-
-	for idx := 0; idx < len(horizontalLines)+len(verticalLines)-1; idx++ {
-		xcount := len(verticalLines) - 1
-		if idx > len(horizontalLines)-1 {
-			xcount = xcount - (idx - len(horizontalLines) + 1)
-		}
-		ycount := min(idx, len(horizontalLines)-1)
-
-		backwardDiagonal[idx] = string(horizontalLines[ycount][xcount])
-
-		xcount -= 1
-		ycount -= 1
-
-		for (ycount >= 0) && (xcount >= 0) {
-			backwardDiagonal[idx] += string(horizontalLines[ycount][xcount])
-			xcount--
-			ycount--
-		}
-	}
-
-	var matches []string
-
-	matches = append(matches, FindMatches(horizontalLines)...)
-	matches = append(matches, FindMatches(verticalLines)...)
-	matches = append(matches, FindMatches(forwardDiagonal)...)
-	matches = append(matches, FindMatches(backwardDiagonal)...)
-
-
-	fmt.Println(len(matches))
 }
 
-func FindMatches(lines []string) []string {
-	var matches []string 
-
-	forKeyWord := regexp.MustCompile(`XMAS`)
-	backKeyWord := regexp.MustCompile(`SAMX`)
-
-	for _, line := range lines {
-		forMatch := forKeyWord.FindAll([]byte(line), -1)
-		backMatch := backKeyWord.FindAll([]byte(line), -1)
-
-		for _, mat := range forMatch {
-			matches = append(matches, string(mat))
-		}
-
-		for _, mat := range backMatch {
-			matches = append(matches, string(mat))
-		}
+func checkHorizontal(lines []string, row int, column int) int {
+	if column > len(lines[row])-4 {
+		return 0
 	}
 
-	return matches
+	word := "X"
+	nextLetters := string(lines[row][column+1]) + string(lines[row][column+2]) + string(lines[row][column+3])
+
+	madeWord := word + string(nextLetters)
+
+	if madeWord == "XMAS" {
+		//fmt.Printf("row %d, column %d: %s\n", row, column, madeWord)
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func checkHorizontalBackwards(lines []string, row int, column int) int {
+	if column < 3 {
+		return 0
+	}
+
+	word := "X"
+	nextLetters := string(lines[row][column-1]) + string(lines[row][column-2]) + string(lines[row][column-3])
+
+	madeWord := word + string(nextLetters)
+
+	if madeWord == "XMAS" {
+		//fmt.Printf("row %d, column %d: %s\n", row, column, madeWord)
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func checkVertical(lines []string, row int, column int) int {
+	if row > len(lines)-4 {
+		return 0
+	}
+
+	word := "X"
+	nextLetters := string(lines[row+1][column]) + string(lines[row+2][column]) + string(lines[row+3][column])
+
+	madeWord := word + string(nextLetters)
+
+	if madeWord == "XMAS" {
+		//fmt.Printf("row %d, column %d: %s\n", row, column, madeWord)
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func checkVerticalBackwards(lines []string, row int, column int) int {
+	if row < 3 {
+		return 0
+	}
+
+	word := "X"
+	nextLetters := string(lines[row-1][column]) + string(lines[row-2][column]) + string(lines[row-3][column])
+
+	madeWord := word + string(nextLetters)
+
+	if madeWord == "XMAS" {
+		//fmt.Printf("row %d, column %d: %s\n", row, column, madeWord)
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func checkForwardDiagonal(lines []string, row int, column int) int {
+	if row < 3 || column > len(lines[row])-4 {
+		return 0
+	}
+
+	word := "X"
+	nextLetters := string(lines[row-1][column+1]) + string(lines[row-2][column+2]) + string(lines[row-3][column+3])
+
+	madeWord := word + string(nextLetters)
+
+	if madeWord == "XMAS" {
+		//fmt.Printf("row %d, column %d: %s\n", row, column, madeWord)
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func checkForwardDiagonalBackwards(lines []string, row int, column int) int {
+	if row > len(lines)-4 || column < 3 {
+		return 0
+	}
+
+	word := "X"
+	nextLetters := string(lines[row+1][column-1]) + string(lines[row+2][column-2]) + string(lines[row+3][column-3])
+
+	madeWord := word + string(nextLetters)
+
+
+	if madeWord == "XMAS" {
+		//fmt.Printf("row %d, column %d: %s\n", row, column, madeWord)
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func checkBackDiagonal(lines []string, row int, column int) int {
+	if row > len(lines)-4 || column > len(lines[row])-4 {
+		return 0
+	}
+
+	word := "X"
+	nextLetters := string(lines[row+1][column+1]) + string(lines[row+2][column+2]) + string(lines[row+3][column+3])
+
+	madeWord := word + string(nextLetters)
+
+	if madeWord == "XMAS" {
+		//fmt.Printf("row %d, column %d: %s\n", row, column, madeWord)
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func checkBackDiagonalBackwards(lines []string, row int, column int) int {
+	if row < 3 || column < 3 {
+		return 0
+	}
+
+	word := "X"
+	nextLetters := string(lines[row-1][column-1]) + string(lines[row-2][column-2]) + string(lines[row-3][column-3])
+
+	madeWord := word + string(nextLetters)
+
+	if madeWord == "XMAS" {
+		//fmt.Printf("row %d, column %d: %s\n", row, column, madeWord)
+		return 1
+	} else {
+		return 0
+	}
 }
